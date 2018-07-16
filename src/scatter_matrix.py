5a04a9742c7d6a3ac0c17fa4d87620c107b39b6f
Mark Lennox <mark@webpusher.ie>
2018-07-16 08:53:09 +0100

    scatter matrix  has better axes and labels now

--- src/scatter_matrix.py
+++ src/scatter_matrix.py
@@ -5,12 +5,20 @@ from pandas.plotting import scatter_matrix
 from loader import load_training, pull_features, pull_features_and_era_label
 
 
+def update_figure(matrix_fig):
+    # rotate x-axis labels 45 degrees and y-axis to horizontal
+    matrix_fig.xaxis.label.set_rotation(45)
+    matrix_fig.yaxis.label.set_rotation(0)
+    matrix_fig.get_yaxis().set_label_coords(-0.6, 0.5)
+    # switch off ticks
+    matrix_fig.set_xticks(())
+    matrix_fig.set_yticks(())
+
+
 def create_scatter_matrix(dataset, filename, color='red'):
     fig, ax = plt.subplots()
-    scatter_matrix(dataset, color=color, ax=ax)
-    # ax.axes.
-    # plt.xticks(rotation='vertical')
-    # plt.yticks(rotation='horizontal')
+    matrix = scatter_matrix(dataset, color=color, ax=ax)
+    [update_figure(m) for m in matrix.reshape(-1)]
     plt.savefig(filename)
     plt.close(fig)
 
@@ -31,6 +39,8 @@ training_data = load_training()
 
 scatter_matrix_variation(
     pull_features(training_data, target_bernie_value=0, for_era=1),
-    ['feature' + str(feature) for feature in range(4, 17)],
+    ['feature4', 'feature5', 'feature6', 'feature14',
+        'feature15', 'feature16', 'feature17'],
+    # ['feature' + str(feature) for feature in range(4, 17)],
     '_era1_4_17',
     frac=1.0)
