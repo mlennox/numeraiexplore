cd0211621fa5344b722538a8f68a3da5aa697c8e
Mark Lennox <mark@webpusher.ie>
2018-07-30 23:11:27 +0100

    adding black linter

--- src/boxplot.py
+++ src/boxplot.py
@@ -7,10 +7,12 @@ training_data = load_training()
 
 
 for era in range(1, 5):
-    X = pull_lots(training_data[training_data['era'] == 'era' + str(era)])
+    X = pull_lots(training_data[training_data["era"] == "era" + str(era)])
     for feature in range(1, 5):
-        ax = sns.catplot(x="target_bernie", y="feature" + str(feature),
-                         kind='box', data=X)
+        ax = sns.catplot(
+            x="target_bernie", y="feature" + str(feature), kind="box", data=X
+        )
         ax.set(ylim=(0.0, 1.0))
-        plt.savefig('images/box/boxplot_era' + str(era) +
-                    '_feature' + str(feature) + '.png')
+        plt.savefig(
+            "images/box/boxplot_era" + str(era) + "_feature" + str(feature) + ".png"
+        )
--- src/scatter_matrix_seaborn.py
+++ src/scatter_matrix_seaborn.py
@@ -5,30 +5,11 @@ from loader import load_training, pull_features, pull_lots
 
 training_data = load_training()
 
-dataset = pull_lots(training_data[training_data['era'] == 'era1'])
+dataset = pull_lots(training_data[training_data["era"] == "era1"])
 
 sns.pairplot(dataset, aspect=1)
-plt.savefig('images/scatter/scatter_matrix_seaborn.png')
+plt.savefig("images/scatter/scatter_matrix_seaborn.png")
 
-sns.pairplot(dataset, aspect=1, hue='target_bernie')
-plt.savefig('images/scatter/scatter_matrix_seaborn_hue.png')
+sns.pairplot(dataset, aspect=1, hue="target_bernie")
+plt.savefig("images/scatter/scatter_matrix_seaborn_hue.png")
 
-#  , 'feature4',
-#  'feature5', 'feature6', 'feature7', 'target_bernie']
-
-# sns.pairplot(X[select_features].sample(
-#     frac=1.0), aspect=1, hue='target_bernie')
-# plt.savefig('images/scatter/scatter_matrix_seaborn_hue.png')
-
-
-# sns.catplot(x="target_bernie", y="feature1",
-#             kind='box', data=X[select_features])
-# plt.savefig('images/scatter/boxplot_feature1.png')
-
-# sns.catplot(x="target_bernie", y="feature2",
-#             kind='box', data=X[select_features])
-# plt.savefig('images/scatter/boxplot_feature2.png')
-
-# sns.catplot(x="target_bernie", y="feature3",
-#             kind='box', data=X[select_features])
-# plt.savefig('images/scatter/boxplot_feature3.png')
